6912a1cbcd22ce572355efdc13af438ea80020bd
Muhammed Fahiz <fahizp3@gmail.com>
2023-04-22 20:51:49 +0530

    SET THE ARTICLE SEARCH FROM UI
    build a style good from for to search the ai based article summarizer

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+import { copy, linkIcon, loader, tick } from "../assets/index.ts";
 
 const Demo = () => {
   return (
-    <div>Demo</div>
-  )
+    <section className='section'>
+      {/* Search */}
+      <div className='search'>
+        <form
+          className='from'
+          // onSubmit={}
+        >
+          <img
+            src={linkIcon}
+            alt='link-icon'
+            className='demo-img'
+          />
+
+          <input
+            type='url'
+            placeholder='Paste the article link'
+            // value={}
+            // onChange={}
+            // onKeyDown={}
+            required
+            className='url_input peer' 
+          />
+          <button
+            type='submit'
+            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
+          >
+            <p>↵</p>
+          </button>
+        </form>
+
+        {/* Browse History */}
+        
+
+      {/* Display Result */}
+     
+      </div>
+    </section>
+  );
 }
 
 export default Demo
